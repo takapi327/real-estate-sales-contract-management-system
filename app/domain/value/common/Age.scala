8f57b13400ed84b303e39bd6971548671d71e050
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-07 17:52:22 +0900

    Fix Age Value Object package name

--- app/domain/value/common/Age.scala
+++ app/domain/value/common/Age.scala
@@ -1,13 +1,12 @@
-package domain.value
+package domain.value.common
 
 import eu.timepit.refined._
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.api.Refined
 
 import io.estatico.newtype.macros.newtype
-import io.estatico.newtype.ops._
 
-package object Common {
+package object age {
 
   type AgeRule = Interval.OpenClosed[0, 150]
   type AgeInt  = Int Refined AgeRule
