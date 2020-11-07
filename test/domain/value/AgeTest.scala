5031d9d60786a340d2dfaa99f6a7f17075483b74
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-07 21:28:06 +0900

    Create Scala Test

---
+++ test/domain/value/AgeTest.scala
@@ -0,0 +1,23 @@
+package domain.value.common.age
+
+import org.scalatest._
+import org.scalatestplus.play._
+
+class AgeTest extends FunSuite {
+
+  test("If you pass 1 for the Age argument, it becomes Right") {
+    assert(Age(1).isRight)
+  }
+
+  test("If you pass 0 for the Age argument, it becomes Left") {
+    assert(Age(0).isLeft)
+  }
+
+  test("If you pass 151 for the Age argument, it becomes Left") {
+    assert(Age(151).isLeft)
+  }
+
+  test("If you pass 150 for the Age argument, it becomes Right") {
+    assert(Age(150).isRight)
+  }
+}
