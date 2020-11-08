1adb9eb2757012ef0b736b139aa9b40ed6da8790
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-08 22:39:14 +0900

    Create Property ArrangementOfRooms Domain Value

---
+++ app/domain/value/property/ArrangementOfRooms.scala
@@ -0,0 +1,23 @@
+package domain.value.property
+
+import eu.timepit.refined._
+import eu.timepit.refined.collection._
+import eu.timepit.refined.string._
+import eu.timepit.refined.api.Refined
+
+import io.estatico.newtype.macros.newtype
+
+package object arrangementofrooms {
+
+  type ArrangementOfRoomsRule   = MatchesRegex["""[1-9]+[A-Z]+[A-Z]"""]
+  type ArrangementOfRoomsString = String Refined ArrangementOfRoomsRule
+
+  @newtype case class ArrangementOfRooms(value: ArrangementOfRoomsString)
+
+  object ArrangementOfRooms {
+    def apply(rawArrangementOfRooms: String): Either[String, ArrangementOfRooms] = {
+      refineV[ArrangementOfRoomsRule](rawArrangementOfRooms).map(ArrangementOfRooms(_))
+    }
+  }
+
+}
