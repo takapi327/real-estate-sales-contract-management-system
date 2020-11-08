8d048936a122f52474561612c2049e4f4261b36f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-08 19:34:40 +0900

    Create Property Price Domain Value

---
+++ app/domain/value/property/Price.scala
@@ -0,0 +1,22 @@
+package domain.value.property
+
+import eu.timepit.refined._
+import eu.timepit.refined.collection._
+import eu.timepit.refined.string._
+import eu.timepit.refined.api.Refined
+
+import io.estatico.newtype.macros.newtype
+
+package object price {
+
+  type PriceRule   = MatchesRegex["""\b\d{1,3}(,\d{3})*\b"""]
+  type PriceString = String Refined PriceRule
+
+  @newtype case class Price(value: PriceString)
+
+  object Price {
+    def apply(rawPrice: String): Either[String, Price] = {
+      refineV[PriceRule](rawPrice).map(Price(_))
+    }
+  }
+}
