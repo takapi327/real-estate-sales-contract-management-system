e545d244a811f766a297c512f9d17b7f02c48b66
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-20 23:58:33 +0900

    Create Employee Model Test

---
+++ test/domain/model/company/EmployeeTest.scala
@@ -0,0 +1,35 @@
+package domain.model.company
+
+import org.scalatest._
+import org.scalatestplus.play._
+
+class EmployeeTest extends FunSuite {
+
+  test("If you pass the Employee model arguments correctly, they will be stored in Valid") {
+    assert(
+      Employee.create(
+        rawFirstName     = "test",
+        rawLastName      = "テスト",
+        rawAge           = 25,
+        rawAddress       = "hogehoge",
+        rawPhoneNumber   = "0901234567",
+        rawEmail         = "test@ezweb.ne.jp",
+        rawLicenseNumber = None
+      ).isValid
+    )
+  }
+
+  test("If you pass the Employee model arguments with LicenseNumber correctly, they will be stored in Valid") {
+    assert(
+      Employee.create(
+        rawFirstName     = "test",
+        rawLastName      = "テスト",
+        rawAge           = 25,
+        rawAddress       = "hogehoge",
+        rawPhoneNumber   = "0901234567",
+        rawEmail         = "test@ezweb.ne.jp",
+        rawLicenseNumber = Some(378792)
+      ).isValid
+    )
+  }
+}
