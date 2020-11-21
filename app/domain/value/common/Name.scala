b75d6ec9ffea65dff7162267f1d950aa32751bd5
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-21 19:05:19 +0900

    Comment out Refined Fix Either.cond Create Name Domain Value

--- app/domain/value/common/Name.scala
+++ app/domain/value/common/Name.scala
@@ -1,5 +1,5 @@
 package domain.value.common
-
+/*
 import eu.timepit.refined._
 import eu.timepit.refined.collection._
 import eu.timepit.refined.boolean._
@@ -25,5 +25,31 @@ package object name {
     def apply(rawLastName: String): Either[String, LastName] = {
       refineV[NameRule](rawLastName).map(LastName(_))
     }
+  }
+}
+*/
+import library.model.{EntityValue, Error}
+
+case class FirstName(value: String) extends EntityValue[String]
+object FirstName extends Error {
+
+  def apply(rawFirstName: String): Either[String, FirstName] = {
+    Either.cond(
+      rawFirstName.isEmpty,
+      new FirstName(rawFirstName),
+      validationError[String](rawFirstName)
+    )
+  }
+}
+
+case class LastName(value: String) extends EntityValue[String]
+object LastName extends Error {
+
+  def apply(rawLastName: String): Either[String, LastName] = {
+    Either.cond(
+      rawLastName.isEmpty,
+      new LastName(rawLastName),
+      validationError[String](rawLastName)
+    )
   }
 }
