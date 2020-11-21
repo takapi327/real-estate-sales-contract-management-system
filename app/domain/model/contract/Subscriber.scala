5479a85ce3905ebbf65afda04edcbe09daa3d0ff
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-21 19:00:26 +0900

    Fix Domain Value Email

--- app/domain/model/contract/Subscriber.scala
+++ app/domain/model/contract/Subscriber.scala
@@ -6,7 +6,6 @@ import cats.data.ValidatedNel
 import cats.implicits._
 
 import domain.value.common._
-import domain.value.common.email._
 import domain.value.common.name._
 import domain.value.common.phone._
 
