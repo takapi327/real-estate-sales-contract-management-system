4f3dd497a72dfd6428e6934449209467ce3a5cbf
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-11-21 18:46:11 +0900

    Comment out Refined Fix Either.cond Create Domain Value

--- app/domain/value/common/Age.scala
+++ app/domain/value/common/Age.scala
@@ -1,5 +1,6 @@
 package domain.value.common
 
+/*
 import eu.timepit.refined._
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.api.Refined
@@ -17,5 +18,20 @@ package object age {
     def apply(rawAge: Int): Either[String, Age] = {
       refineV[AgeRule](rawAge).map(Age(_))
     }
+  }
+}
+*/
+import library.model.{EntityValue, Error}
+
+case class Age(value: Int) extends EntityValue[Int]
+
+object Age extends Error {
+
+  def apply(rawAge: Int): Either[String, Age] = {
+    Either.cond(
+      rawAge >= 1 && rawAge <= 150,
+      new Age(rawAge),
+      validationError[Int](rawAge)
+    )
   }
 }
