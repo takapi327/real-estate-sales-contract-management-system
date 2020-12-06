b268ee9ecc757ebcbd77789cd93e64ef8a40d043
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-06 15:59:42 +0900

    Create ContractInformationTable

---
+++ app/interface-adapter/gateway/dao/contract/ContractInformation.scala
@@ -0,0 +1,34 @@
+package gateway.dao
+
+import slick.lifted.Tag
+
+import library.backend.SlickTable
+import library.backend.SlickDatabaseConfig
+
+import domain.model.company.Employee
+import domain.model.property.Property
+import domain.model.contract._
+
+import ContractInformation._
+class ContractInformationTable(tag: Tag) extends SlickTable[ContractInformation](tag, "contract_information") {
+
+  import api._
+  import mapping._
+
+  /* @1 */ def id         = column[ContractInformation.Id]  ("id", O.PrimaryKey)
+  /* @2 */ def propertyId = column[Property.Id]             ("property_id")
+  /* @3 */ def contents   = column[String]                  ("contents")
+
+  type TableElementTuple = (
+    ContractInformation.Id, Property.Id, String
+  )
+
+  def * = (id, propertyId, contents) .<> (
+    (x: TableElementTuple) => ContractInformation(
+      x._1, x._2, Contents(x._3)
+    ),
+    (v: ContractInformation) => ContractInformation.unapply(v).map {t => (
+      t._1, t._2, t._3.value
+    )}
+  )
+}
