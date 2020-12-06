f8eb876eb3938c425fe832dbfd2bab20f2dbb45a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-06 15:57:53 +0900

    Create PropertyTable

---
+++ app/interface-adapter/gateway/dao/property/Property.scala
@@ -0,0 +1,43 @@
+package gateway.dao
+
+import slick.lifted.Tag
+
+import library.backend.SlickTable
+import library.backend.SlickDatabaseConfig
+
+import domain.model.property.Property
+
+import domain.value.common.age._
+import domain.value.common.address._
+
+import domain.value.property._
+import domain.value.property.price._
+import domain.value.property.arrangementofrooms._
+
+class PropertyTable(tag: Tag) extends SlickTable[Property](tag, "property") {
+
+  import api._
+  import mapping._
+
+  /* @1 */ def id                 = column[Property.Id]                ("property_id", O.PrimaryKey)
+  /* @2 */ def address            = column[AddressString]              ("address")
+  /* @3 */ def types              = column[Int]                        ("types")
+  /* @4 */ def price              = column[PriceString]                ("price")
+  /* @5 */ def age                = column[AgeInt]                     ("age")
+  /* @6 */ def structure          = column[Int]                        ("structure")
+  /* @7 */ def arrangementOfRooms = column[ArrangementOfRoomsString]   ("arrangement_of_rooms")
+
+  type TableElementTuple = (
+    Property.Id, AddressString, Int,  PriceString,
+    AgeInt, Int, ArrangementOfRoomsString
+  )
+
+  def * = (id, address, types, price, age, structure, arrangementOfRooms) .<> (
+    (x: TableElementTuple) => Property(
+      x._1, Address(x._2), Type.find(x._3), Price(x._4), Age(x._5), Structure.find(x._6), ArrangementOfRooms(x._7)
+    ),
+    (v: Property) => Property.unapply(v).map {t => (
+      t._1, t._2.value, t._3.code, t._4.value, t._5.value, t._6.code, t._7.value
+    )}
+  )
+}
