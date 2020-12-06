7ff404df057003bd9a5a7edbc20c4125e17b4651
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-06 15:59:03 +0900

    Create ContractOfBuyTable

---
+++ app/interface-adapter/gateway/dao/contract/ContractOfBuy.scala
@@ -0,0 +1,32 @@
+package gateway.dao
+
+import slick.lifted.Tag
+
+import library.backend.SlickTable
+import library.backend.SlickDatabaseConfig
+
+import domain.model.company.Employee
+import domain.model.contract.{ContractOfBuy, ContractInformation, Subscriber}
+
+class ContractOfBuyTable(tag: Tag) extends SlickTable[ContractOfBuy](tag, "contract_of_buy") {
+
+  import api._
+  import mapping._
+
+  /* @1 */ def contractInformationId = column[ContractInformation.Id]  ("contract_information_id", O.PrimaryKey)
+  /* @2 */ def subscriberId          = column[Subscriber.Id]           ("subscriber_id")
+  /* @3 */ def employeeId            = column[Employee.Id]             ("employee_id")
+
+  type TableElementTuple = (
+    ContractInformation.Id, Subscriber.Id, Employee.Id,
+  )
+
+  def * = (contractInformationId, subscriberId, employeeId) .<> (
+    (x: TableElementTuple) => ContractOfBuy(
+      x._1, x._2, x._3
+    ),
+    (v: ContractOfBuy) => ContractOfBuy.unapply(v).map {t => (
+      t._1, t._2, t._3
+    )}
+  )
+}
