e671ceab4adb0d0d443a585712bb56afb166d20c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-19 18:21:17 +0900

    Create ContractOfSale sql

---
+++ sql/ddl/create/ContractOfSale.sql
@@ -0,0 +1,8 @@
+--- !Ups
+
+CREATE TABLE contract_of_sale (
+  contract_information_id BIGINT UNSIGNED NOT NULL,
+  subscriber_id           BIGINT UNSIGNED NOT NULL,
+  employee_id             BIGINT UNSIGNED NOT NULL,
+  FOREIGN KEY (contract_information_id, subscriber_id, employee_id),
+);
