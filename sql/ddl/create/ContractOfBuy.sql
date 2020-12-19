9e1d484dbc10bfa4089c25019a81db5d31d85c0c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-19 18:21:28 +0900

    Rename ContractOfBuy sql

--- sql/ddl/create/ContractOfBuy.sql
+++ sql/ddl/create/ContractOfBuy.sql
@@ -1,6 +1,6 @@
 --- !Ups
 
-CREATE TABLE property (
+CREATE TABLE contract_of_buy (
   contract_information_id BIGINT UNSIGNED NOT NULL,
   subscriber_id           BIGINT UNSIGNED NOT NULL,
   employee_id             BIGINT UNSIGNED NOT NULL,
