819cfc07650d2f0a86ea8266d36f2d85b75943a9
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-19 17:38:37 +0900

    Create ContractOfBuyRepository

---
+++ app/interface-adapter/gateway/repository/contract/ContractOfBuy.scala
@@ -0,0 +1,36 @@
+package gateway.repository 
+
+import scala.concurrent.{ExecutionContext, Future}
+import javax.inject.Inject
+
+import library.backend.SlickRepository
+
+import domain.model.contract.{ContractOfBuy, ContractInformation}
+import domain.repository.ContractOfBuyRepository
+
+import gateway.dao.ContractOfBuyTable
+
+class ContractOfBuyRepositoryImpl @Inject()(
+  implicit ec: ExecutionContext
+) extends ContractOfBuyRepository
+  with    SlickRepository {
+
+  import api._
+
+  lazy val contractOfBuyTable = TableQuery[ContractOfBuyTable]
+
+  def add(contractOfBuy: ContractOfBuy): Future[ContractOfBuy] = {
+    db.run(
+      (contractOfBuyTable returning contractOfBuyTable) += contractOfBuy
+    )
+  }
+
+  def findByContractInformationId(
+    contractInformationId: ContractInformation.Id
+  ): Future[Option[ContractOfBuy]] = {
+    db.run(
+      contractOfBuyTable.filter(_.contractInformationId === contractInformationId)
+        .result.headOption
+    )
+  }
+}
