39426943880d3dd19e0ed0b7a715d8a0342c3545
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-23 01:00:50 +0900

    Change Test FunSuite -> PlaySpec in PropertyTest

--- test/domain/model/property/PropertyTest.scala
+++ test/domain/model/property/PropertyTest.scala
@@ -5,17 +5,15 @@ import java.time.LocalDate
 import org.scalatest._
 import org.scalatestplus.play._
 
-class PropertyTest extends FunSuite {
-  test("If you pass the Property model arguments correctly, they will be stored in Valid") {
-    assert(
-      Property.create(
-        rawAddress            = "hogehoge",
-        rawTypeCode           = 2,
-        rawPrice              = "20,000,000",
-        rawDateBuilt          = LocalDate.now(),
-        rawStructureCode      = 4,
-        rawArrangementOfRooms = "3LDK"
-      ).isValid
-    )
+class PropertyTest extends PlaySpec {
+  "If you pass the Property model arguments correctly, they will be stored in Valid" in {
+    Property.create(
+      rawAddress            = "hogehoge",
+      rawTypeCode           = 2,
+      rawPrice              = "20,000,000",
+      rawDateBuilt          = LocalDate.now(),
+      rawStructureCode      = 4,
+      rawArrangementOfRooms = "3LDK"
+    ).isValid mustBe true
   }
 }
