900dbcaaf80ef87893a09844818ec73f05c3e9e5
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-23 01:01:56 +0900

    Change Test FunSuite -> PlaySpec in SubscriberTest

--- test/domain/model/contract/SubscriberTest.scala
+++ test/domain/model/contract/SubscriberTest.scala
@@ -3,21 +3,19 @@ package domain.model.contract
 import org.scalatest._
 import org.scalatestplus.play._
 
-class SubscriberTest extends FunSuite {
+class SubscriberTest extends PlaySpec {
 
-  test("If you pass the Subscriber model arguments correctly, they will be stored in Valid") {
-    assert(
-      Subscriber.create(
-        rawFirstName   = "test",
-        rawLastName    = "テスト",
-        rawBirthYear   = 1995,
-        rawBirthMonth  = 7,
-        rawBirthDay    = 15,
-        rawAddress     = "hogehoge",
-        rawPhoneNumber = "0901234567",
-        rawEmail       = "test@ezweb.ne.jp"
-      ).isValid
-    )
+  "If you pass the Subscriber model arguments correctly, they will be stored in Valid" in {
+    Subscriber.create(
+      rawFirstName   = "test",
+      rawLastName    = "テスト",
+      rawBirthYear   = 1995,
+      rawBirthMonth  = 7,
+      rawBirthDay    = 15,
+      rawAddress     = "hogehoge",
+      rawPhoneNumber = "0901234567",
+      rawEmail       = "test@ezweb.ne.jp"
+    ).isValid mustBe true
   }
 
 }
