c0c9632044b0cfcda995dc55f995edf663ca82c2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-24 22:37:08 +0900

    Fix Employee sql

--- sql/ddl/create/Employee.sql
+++ sql/ddl/create/Employee.sql
@@ -1,16 +1,16 @@
 --- !Ups
 
 CREATE TABLE `employee` (
-  `employee_id`    BIGINT(20)   UNSIGNED NOT NULL,
+  `employee_id`    VARCHAR(255)          NOT NULL,
   `first_name`     VARCHAR(255)          NOT NULL,
   `last_name`      VARCHAR(255)          NOT NULL,
-  `birth_year`     TINYINT      UNSIGNED NOT NULL,
+  `birth_year`     SMALLINT     UNSIGNED NOT NULL,
   `birth_month`    TINYINT      UNSIGNED NOT NULL,
   `birth_day`      TINYINT      UNSIGNED NOT NULL,
   `address`        VARCHAR(255)          NOT NULL,
-  `phone_number`   TINYINT      UNSIGNED NOT NULL,
-  `email`          VARCHAR(255),
-  `license_number` BIGINT(20)   UNSIGNED,
+  `phone_number`   VARCHAR(255)          NOT NULL,
+  `email`          VARCHAR(255)          NOT NULL,
+  `license_number` BIGINT(20)   UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`employee_id`),
   UNIQUE KEY (`email`, `license_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
