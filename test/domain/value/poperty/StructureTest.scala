a7655f6c7504e3d26ebbfce3d4c417ff1f712d0a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-26 22:44:34 +0900

    Fix StructureTest

--- test/domain/value/poperty/StructureTest.scala
+++ test/domain/value/poperty/StructureTest.scala
@@ -94,135 +94,129 @@ class StructureTest extends PlaySpec {
   /**
    * HEAVY_STEEL_CONSTRUCTION Test
    */
-  /*
-  test("The HEAVY_STEEL_CONSTRUCTION object upperName is the same as the specified string") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.upperName == "HEAVY_STEEL_CONSTRUCTION")
+  "The HEAVY_STEEL_CONSTRUCTION object upperName is the same as the specified string" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.upperName mustBe "HEAVY_STEEL_CONSTRUCTION"
   }
 
-  test("The HEAVY_STEEL_CONSTRUCTION object upperName is not the same as the specified string") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.upperName != "REINFORCED_CONCRETE_CONSTRUCTION")
+  "The HEAVY_STEEL_CONSTRUCTION object upperName is not the same as the specified string" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.upperName must not be "REINFORCED_CONCRETE_CONSTRUCTION"
   }
 
-  test("The HEAVY_STEEL_CONSTRUCTION object lowerName is the same as the specified stwring") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.lowerName == "heavy_steel_construction")
+  "The HEAVY_STEEL_CONSTRUCTION object lowerName is the same as the specified stwring" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.lowerName mustBe "heavy_steel_construction"
   }
 
-  test("The HEAVY_STEEL_CONSTRUCTION object lowerName is not the same as the specified string") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.lowerName != "reinforced_concrete_construction")
+  "The HEAVY_STEEL_CONSTRUCTION object lowerName is not the same as the specified string" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.lowerName must not be "reinforced_concrete_construction"
   }
 
-  test("HEAVY_STEEL_CONSTRUCTION code will be the same as the specified number") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.code == 2)
+  "HEAVY_STEEL_CONSTRUCTION code will be the same as the specified number" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.code mustBe 2
   }
 
-  test("HEAVY_STEEL_CONSTRUCTION code will not be the same as the specified number") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.code != 3)
+  "HEAVY_STEEL_CONSTRUCTION code will not be the same as the specified number" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.code must not be 3
   }
 
-  test("The HEAVY_STEEL_CONSTRUCTION object nameSet is the same as the specified name set") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.nameSet == ("HEAVY_STEEL_CONSTRUCTION", "heavy_steel_construction", "重量鉄骨造"))
+  "The HEAVY_STEEL_CONSTRUCTION object nameSet is the same as the specified name set" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.nameSet mustBe ("HEAVY_STEEL_CONSTRUCTION", "heavy_steel_construction", "重量鉄骨造")
   }
 
-  test("The HEAVY_STEEL_CONSTRUCTION object detailName is not the same as the specified name set") {
-    assert(Structure.HEAVY_STEEL_CONSTRUCTION.nameSet != ("REINFORCED_CONCRETE_CONSTRUCTION", "reinforced_concrete_construction", "鉄筋コンクリート造"))
+  "The HEAVY_STEEL_CONSTRUCTION object detailName is not the same as the specified name set" in {
+    Structure.HEAVY_STEEL_CONSTRUCTION.nameSet must not be ("REINFORCED_CONCRETE_CONSTRUCTION", "reinforced_concrete_construction", "鉄筋コンクリート造")
   }
 
-  test("The Type obtained from the Type search is the same as the specified HEAVY_STEEL_CONSTRUCTION") {
-    assert(Structure.find(2) == Structure.HEAVY_STEEL_CONSTRUCTION)
+  "The Type obtained from the Type search is the same as the specified HEAVY_STEEL_CONSTRUCTION" in {
+    Structure.find(2) mustBe Structure.HEAVY_STEEL_CONSTRUCTION
   }
 
-  test("The Type obtained from the Type search is not the same as the specified HEAVY_STEEL_CONSTRUCTION") {
-    assert(Structure.find(3) != Structure.HEAVY_STEEL_CONSTRUCTION)
+  "The Type obtained from the Type search is not the same as the specified HEAVY_STEEL_CONSTRUCTION" in {
+    Structure.find(3) must not be Structure.HEAVY_STEEL_CONSTRUCTION
   }
-  */
 
   /**
    * REINFORCED_CONCRETE_CONSTRUCTION Test
    */
-  /*
-  test("The REINFORCED_CONCRETE_CONSTRUCTION object upperName is the same as the specified string") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.upperName == "REINFORCED_CONCRETE_CONSTRUCTION")
+  "The REINFORCED_CONCRETE_CONSTRUCTION object upperName is the same as the specified string" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.upperName mustBe "REINFORCED_CONCRETE_CONSTRUCTION"
   }
 
-  test("The REINFORCED_CONCRETE_CONSTRUCTION object upperName is not the same as the specified string") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.upperName != "STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION")
+  "The REINFORCED_CONCRETE_CONSTRUCTION object upperName is not the same as the specified string" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.upperName must not be "STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION"
   }
 
-  test("The REINFORCED_CONCRETE_CONSTRUCTION object lowerName is the same as the specified stwring") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.lowerName == "reinforced_concrete_construction")
+  "The REINFORCED_CONCRETE_CONSTRUCTION object lowerName is the same as the specified stwring" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.lowerName mustBe "reinforced_concrete_construction"
   }
 
-  test("The REINFORCED_CONCRETE_CONSTRUCTION object lowerName is not the same as the specified string") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.lowerName != "steel_framed_reinfoced_concrete_construction")
+  "The REINFORCED_CONCRETE_CONSTRUCTION object lowerName is not the same as the specified string" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.lowerName must not be "steel_framed_reinfoced_concrete_construction"
   }
 
-  test("REINFORCED_CONCRETE_CONSTRUCTION code will be the same as the specified number") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.code == 3)
+  "REINFORCED_CONCRETE_CONSTRUCTION code will be the same as the specified number" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.code mustBe 3
   }
 
-  test("REINFORCED_CONCRETE_CONSTRUCTION code will not be the same as the specified number") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.code != 4)
+  "REINFORCED_CONCRETE_CONSTRUCTION code will not be the same as the specified number" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.code must not be 4
   }
 
-  test("The REINFORCED_CONCRETE_CONSTRUCTION object nameSet is the same as the specified name set") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.nameSet == ("REINFORCED_CONCRETE_CONSTRUCTION", "reinforced_concrete_construction", "鉄筋コンクリート造"))
+  "The REINFORCED_CONCRETE_CONSTRUCTION object nameSet is the same as the specified name set" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.nameSet mustBe ("REINFORCED_CONCRETE_CONSTRUCTION", "reinforced_concrete_construction", "鉄筋コンクリート造")
   }
 
-  test("The REINFORCED_CONCRETE_CONSTRUCTION object detailName is not the same as the specified name set") {
-    assert(Structure.REINFORCED_CONCRETE_CONSTRUCTION.nameSet != ("STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION", "steel_framed_reinfoced_concrete_construction", "鉄骨鉄筋コンクリート造"))
+  "The REINFORCED_CONCRETE_CONSTRUCTION object detailName is not the same as the specified name set" in {
+    Structure.REINFORCED_CONCRETE_CONSTRUCTION.nameSet must not be ("STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION", "steel_framed_reinfoced_concrete_construction", "鉄骨鉄筋コンクリート造")
   }
 
-  test("The Type obtained from the Type search is the same as the specified REINFORCED_CONCRETE_CONSTRUCTION") {
-    assert(Structure.find(3) == Structure.REINFORCED_CONCRETE_CONSTRUCTION)
+  "The Type obtained from the Type search is the same as the specified REINFORCED_CONCRETE_CONSTRUCTION" in {
+    Structure.find(3) mustBe Structure.REINFORCED_CONCRETE_CONSTRUCTION
   }
 
-  test("The Type obtained from the Type search is not the same as the specified REINFORCED_CONCRETE_CONSTRUCTION") {
-    assert(Structure.find(4) != Structure.REINFORCED_CONCRETE_CONSTRUCTION)
+  "The Type obtained from the Type search is not the same as the specified REINFORCED_CONCRETE_CONSTRUCTION" in {
+    Structure.find(4) must not be Structure.REINFORCED_CONCRETE_CONSTRUCTION
   }
-  */
 
   /**
    * STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION Test
    */
-  /*
-  test("The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object upperName is the same as the specified string") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.upperName == "STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION")
+  "The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object upperName is the same as the specified string" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.upperName mustBe "STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION"
   }
 
-  test("The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object upperName is not the same as the specified string") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.upperName != "WOODWORK")
+  "The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object upperName is not the same as the specified string" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.upperName must not be "WOODWORK"
   }
 
-  test("The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object lowerName is the same as the specified stwring") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.lowerName == "steel_framed_reinfoced_concrete_construction")
+  "The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object lowerName is the same as the specified stwring" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.lowerName mustBe "steel_framed_reinfoced_concrete_construction"
   }
 
-  test("The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object lowerName is not the same as the specified string") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.lowerName != "woodwork")
+  "The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object lowerName is not the same as the specified string" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.lowerName must not be "woodwork"
   }
 
-  test("STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION code will be the same as the specified number") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.code == 4)
+  "STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION code will be the same as the specified number" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.code mustBe 4
   }
 
-  test("STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION code will not be the same as the specified number") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.code != 0)
+  "STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION code will not be the same as the specified number" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.code must not be 0
   }
 
-  test("The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object nameSet is the same as the specified name set") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.nameSet == ("STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION", "steel_framed_reinfoced_concrete_construction", "鉄骨鉄筋コンクリート造"))
+  "The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object nameSet is the same as the specified name set" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.nameSet mustBe ("STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION", "steel_framed_reinfoced_concrete_construction", "鉄骨鉄筋コンクリート造")
   }
 
-  test("The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object detailName is not the same as the specified name set") {
-    assert(Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.nameSet != ("WOODWORK", "woodwork", "木造"))
+  "The STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION object detailName is not the same as the specified name set" in {
+    Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION.nameSet must not be ("WOODWORK", "woodwork", "木造")
   }
 
-  test("The Type obtained from the Type search is the same as the specified STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION") {
-    assert(Structure.find(4) == Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION)
+  "The Type obtained from the Type search is the same as the specified STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION" in {
+    Structure.find(4) mustBe Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION
   }
 
-  test("The Type obtained from the Type search is not the same as the specified STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION") {
-    assert(Structure.find(0) != Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION)
+  "The Type obtained from the Type search is not the same as the specified STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION" in {
+    Structure.find(0) must not be Structure.STEEL_FRAMED_REINFOCED_CONCRETE_CONSTRUCTION
   }
-  */
 }
