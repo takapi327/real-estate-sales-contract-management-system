bd60c91d05b38aa199c61e4c0cefff7c8bdd47a8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-26 22:21:28 +0900

    Fix ddl sql column type

--- sql/ddl/create/Property.sql
+++ sql/ddl/create/Property.sql
@@ -4,9 +4,9 @@ CREATE TABLE `property` (
   `property_id`           VARCHAR(255)          NOT NULL,
   `address`               VARCHAR(255)          NOT NULL,
   `types`                 TINYINT      UNSIGNED NOT NULL,
-  `price`                 BIGINT(20)   UNSIGNED NOT NULL,
+  `price`                 VARCHAR(255)          NOT NULL,
   `date_built`            DATE                  NOT NULL,
   `structure`             TINYINT      UNSIGNED NOT NULL,
-  `arrangement_of_rooms`  TINYINT      UNSIGNED NOT NULL,
+  `arrangement_of_rooms`  VARCHAR(255)          NOT NULL,
   PRIMARY KEY (`property_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
