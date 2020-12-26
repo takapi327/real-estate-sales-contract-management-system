2e2bb22ba42c374891e92d63f7128aff2b1a25ba
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-26 22:21:55 +0900

    Create interface-adapter PropertyTest

---
+++ test/interface-adapter/gateway/repository/property/PropertyTest.scala
@@ -0,0 +1,51 @@
+package gateway.repository 
+
+import java.util.UUID
+import java.time.LocalDate
+
+import org.scalatest._
+import org.scalatestplus.play._
+
+import cats.data.Validated.{Invalid, Valid}
+import cats.implicits._
+
+import domain.model.property.Property
+
+import library.backend.SlickRepositorySpec
+import gateway.dao.PropertyTable
+
+class PropertyRepositorySpec extends SlickRepositorySpec {
+
+  private val repository = fakeApplication.injector.instanceOf[PropertyRepositoryImpl]
+
+  afterAll()
+
+  "PropertyRepositoryImpl Test" should {
+
+    "Can store records in DB" in {
+
+      val property = Property.create(
+        rawAddress            = "hogehoge",
+        rawTypeCode           = 2,
+        rawPrice              = "20,000,000",
+        rawDateBuilt          = LocalDate.now(),
+        rawStructureCode      = 4,
+        rawArrangementOfRooms = "3LDK"
+      )
+
+      property match {
+        case Valid(e)   => repository.add(e).futureValue mustBe 1
+        case Invalid(e) => fail("Not Create Property")
+      }
+
+    }
+
+    "Can retrieve records from DB by ID" in {
+      val propertyId: Property.Id = Property.Id(UUID.fromString("2d407609-4ccd-408e-bd9e-d9f13b7757a8"))
+      (repository.findById(propertyId).futureValue match {
+        case Some(e) => true
+        case None    => false
+      }) mustBe true
+    }
+  }
+}
