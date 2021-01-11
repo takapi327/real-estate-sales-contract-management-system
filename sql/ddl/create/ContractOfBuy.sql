a8d3d181991d3141da4eaee69e709a1f081ceb77
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-01-11 22:14:54 +0900

    Fix Table id

--- sql/ddl/create/ContractOfBuy.sql
+++ sql/ddl/create/ContractOfBuy.sql
@@ -5,6 +5,6 @@ CREATE TABLE `contract_of_buy` (
   `subscriber_id`           VARCHAR(255) NOT NULL,
   `employee_id`             VARCHAR(255) NOT NULL,
   FOREIGN KEY (`contract_information_id`) REFERENCES `contract_information`(`contract_information_id`),
-  FOREIGN KEY (`subscriber_id`)           REFERENCES `subscriber`(`subscriber_id`),
-  FOREIGN KEY (`employee_id`)             REFERENCES `employee`(`employee_id`)
+  FOREIGN KEY (`subscriber_id`)           REFERENCES `subscriber`(`id`),
+  FOREIGN KEY (`employee_id`)             REFERENCES `employee`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- sql/ddl/create/EmployeeDetail.sql
+++ sql/ddl/create/EmployeeDetail.sql
@@ -9,6 +9,6 @@ CREATE TABLE `employee_detail` (
   `phone_number`   VARCHAR(255)          NOT NULL,
   `email`          VARCHAR(255)          NOT NULL,
   `license_number` BIGINT(20)   UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (`employee_id`),
+  FOREIGN KEY (`employee_id`) REFERENCES `employee`(`id`),
   UNIQUE KEY (`email`, `phone_number`, `license_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
