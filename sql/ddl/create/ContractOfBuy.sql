fd6eada4b4634df3a967042fa6bf1caf2c2e955d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-01-12 00:20:02 +0900

    Fix sql ddl file

--- sql/ddl/create/ContractOfBuy.sql
+++ sql/ddl/create/ContractOfBuy.sql
@@ -4,7 +4,7 @@ CREATE TABLE `contract_of_buy` (
   `contract_information_id` VARCHAR(255) NOT NULL,
   `subscriber_id`           VARCHAR(255) NOT NULL,
   `employee_id`             VARCHAR(255) NOT NULL,
-  FOREIGN KEY (`contract_information_id`) REFERENCES `contract_information`(`contract_information_id`),
+  FOREIGN KEY (`contract_information_id`) REFERENCES `contract_information`(`id`),
   FOREIGN KEY (`subscriber_id`)           REFERENCES `subscriber`(`id`),
   FOREIGN KEY (`employee_id`)             REFERENCES `employee`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
